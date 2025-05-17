85653296ca639e694fbaef023369a3a3fb6c944c
ameron <ameron@utu.fi>
2025-05-17 09:59:05 +0300

    Included backend

---
+++ server/src/server.ts
@@ -0,0 +1,21 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+const PORT =  5000
+
+app.use(cors())
+app.use(express.json())
+
+
+app.get('/', (_req, res) => {
+  res.send('Server is running')
+})
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`)
+})
